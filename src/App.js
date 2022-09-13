0928e218abc6a3519bad628ae4dc05e2a05c23e1
Adam Parrish <aparrish@neosavvy.com>
2022-09-13 13:04:04 -0400

    Initial Dashboard layout

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <iframe
+            className="iframe"
+            src={"https://app.sigmacomputing.com/embed/1-1gxjAXZWXgWQYjxVD13m5w"}/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
