67b66ab60e687f70c4fc4d35b3ee3eac5e6c15f2
Adam Parrish <aparrish@neosavvy.com>
2022-09-23 08:42:35 -0400

    Adding an Explore Dashboard
    - Adds the ability to get to our Explore dashboard

--- src/cryptos/CryptoDashboard.js
+++ src/cryptos/CryptoDashboard.js
@@ -1,12 +1,12 @@
 import { useParams } from "@reach/router";
-import { ALL_CRYPTO } from "../PathConstants";
+import { EVERYTHING } from "../PathConstants";
 
 const CryptoDashboard = () => {
   const params = useParams();
   console.log("params=", params);
   let { key } = params;
   console.log("key=", key);
-  let selectedCrypto = ALL_CRYPTO.find((item) => {
+  let selectedCrypto = EVERYTHING.find((item) => {
     return item.path === key;
   });
   console.log("selected=", selectedCrypto);
