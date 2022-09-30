4731ddd899254882f4a239941b66dbb3cc127167
Adam Parrish <aparrish@neosavvy.com>
2022-09-30 09:13:02 -0400

    Adding Github instead of Discord

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 export default () => {
   return (
     <iframe
-      src={"https://app.sigmacomputing.com/embed/1-1gxjAXZWXgWQYjxVD13m5w"}
+      src={"https://app.sigmacomputing.com/embed/1-6XMvyHN09r7IL5GEtWVNoF"}
     />
   );
 };
